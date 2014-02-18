7c189d938251af86068f53c3c2bb3e25d573c0a2
Sumit Arora <er.sumitarora@gmail.com>
2014-02-18 18:49:51 -0500

    Adding test configuration and test classes

--- src/test/java/com/crimealert/init/BaseTestCase.java
+++ src/test/java/com/crimealert/init/BaseTestCase.java
@@ -3,11 +3,14 @@ package com.crimealert.init;
 import lombok.extern.slf4j.Slf4j;
 
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import com.crimealert.data.DummyDataGenerator;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
@@ -16,6 +19,9 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @ActiveProfiles("test")
 @Slf4j
 public abstract class BaseTestCase {
+	
+	@Autowired
+	public DummyDataGenerator ddg;
 	
 	public BaseTestCase() {
 		System.setProperty("spring.profiles.active", "test");
--- src/test/java/com/crimealert/service/TestShopService.java
+++ src/test/java/com/crimealert/service/TestShopService.java
@@ -1,5 +1,6 @@
 package com.crimealert.service;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
@@ -10,6 +11,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.crimealert.exception.ShopNotFound;
 import com.crimealert.init.BaseTestCase;
 import com.crimealert.model.Shop;
 
@@ -19,20 +21,72 @@ public class TestShopService extends BaseTestCase {
 	@Autowired
 	private ShopService shopService;
 
-  @Test
-  public void testRoleCreate() {
-   log.debug("Creating new Role");
-   int i = 10;
-   
-   final Shop s = new Shop();
-   s.setName("Shop Nameaaa");
-   s.setEmplNumber(12);
-   shopService.create(s);
-   
-   final List<Shop> shops = shopService.findAll();
-   assertNotNull(shops);
-   assertTrue(shops.size() > 0);
-   
-  }
+	@Test
+	public void testShopCreate() {
+		log.debug("testing create shop");
+		final Shop s = shopService.create(ddg.createShop());
+		assertTrue(s.getId() > 0);
+		
+		final List<Shop> shops = shopService.findAll();
+		assertNotNull(shops);
+		assertTrue(shops.size() > 0);
+		assertEquals(s.getName(), ddg.createShop().getName());
+		assertEquals(s.getEmplNumber(), ddg.createShop().getEmplNumber());
+	}
 
+	@Test
+	public void testShopDelete() throws ShopNotFound {
+		log.debug("testing delete shop");
+		final Shop s = shopService.create(ddg.createShop());
+		assertTrue(s.getId() > 0);
+		
+		shopService.delete(s.getId());
+		
+		final Shop s2 = shopService.findById(s.getId());
+		assertTrue(s2 == null);		
+	}
+	
+	@Test
+	public void testFindAll() {
+		log.debug("testing find all shops");
+		final Shop s = shopService.create(ddg.createShop());
+		assertTrue(s.getId() > 0);
+		
+		final List<Shop> shops = shopService.findAll();
+		assertNotNull(shops);
+		assertTrue(shops.size() > 0);
+	}
+	
+	@Test
+	public void testShopUpdate() throws ShopNotFound {
+		log.debug("testing shop update");
+		final Shop s = shopService.create(ddg.createShop());
+		assertTrue(s.getId() > 0);
+		
+		s.setName("updated name");
+		s.setEmplNumber(123);
+		shopService.update(s);
+		
+		final List<Shop> shops = shopService.findAll();
+		assertNotNull(shops);
+		assertTrue(shops.size() > 0);
+		
+		assertEquals(s.getId(), shops.get(0).getId());
+		assertEquals(s.getName(), shops.get(0).getName());
+		assertEquals(s.getEmplNumber(), shops.get(0).getEmplNumber());		
+	}
+	
+	@Test
+	public void testFindById() {
+		log.debug("testing find shop by id");
+		final Shop s = shopService.create(ddg.createShop());
+		assertTrue(s.getId() > 0);
+		
+		final Shop s2 = shopService.findById(s.getId());
+		assertNotNull(s2);
+		assertEquals(s.getId(), s2.getId());
+		assertEquals(s.getName(), s2.getName());
+		assertEquals(s.getEmplNumber(), s2.getEmplNumber());
+	}
+	
 }
