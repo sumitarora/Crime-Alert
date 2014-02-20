754871f381fba5559c422203e7be164cb7c37205
Deepanshu7016 <deepanshu7016@gmail.com>
2014-02-20 09:47:57 -0500

    Adding the Complaint Controller and create complaint jsp page and configuring url mappings.

---
+++ src/main/java/com/crimealert/controller/ComplaintController.java
@@ -0,0 +1,22 @@
+package com.crimealert.controller;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping(value="/complaint")
+@Slf4j
+public class ComplaintController {
+
+	@RequestMapping(value="/complaint-create",method=RequestMethod.GET)
+	public ModelAndView sayHello(){
+		ModelAndView mv=new ModelAndView("complaint/complaint-create");
+		return mv;
+		
+	}
+}
+
---
+++ src/main/webapp/WEB-INF/views/hello.jsp
@@ -0,0 +1,11 @@
+<%@ include file="common/header.jsp"%>
+
+
+<h1>hello</h1>
+
+
+
+
+
+
+<%@ include file="common/footer.jsp"%>
